c91118bfa7607fa6fc635b48b6c06d9cde56c858
safafofficial@gmail.com <safafofficial@gmail.com>
2023-09-20 00:08:26 +0530

    added success sweet alerts on student, parent dashboards login (admin already done)

--- Hostel_Management_System_Project/Login.aspx.cs
+++ Hostel_Management_System_Project/Login.aspx.cs
@@ -14,7 +14,12 @@ namespace Hostel_Management_System_Project
         SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-JRHVVPL\SQLEXPRESS;Initial Catalog=hostel_db;Integrated Security=True");
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack) { }
+            if (!IsPostBack) 
+            {
+                //to get a fresh page by removing the cache on browser back button
+                Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                Response.Cache.SetNoStore();
+            }
         }
 
         protected void btn_Login_Click(object sender, EventArgs e)
--- Hostel_Management_System_Project/StudentDashboard.aspx.cs
+++ Hostel_Management_System_Project/StudentDashboard.aspx.cs
@@ -11,7 +11,29 @@ namespace Hostel_Management_System_Project
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!IsPostBack) 
+            {
+                // Check if the SweetAlert has already been shown
+                if (Session["SweetAlertShown"] == null)
+                {
+                    // After successful login, get the user's full name (e.g., fullName) and show SweetAlert
+                    string script = @"
+                    <script type='text/javascript'>
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Login Successful',
+                            text: 'Welcome, " + Session["name"] + @"!',
+                            showConfirmButton: false,
+                            timer: 2000
+                        });
+                    </script>";
 
+                    ClientScript.RegisterStartupScript(this.GetType(), "ShowSuccessAlert", script);
+
+                    // Set the session variable to indicate that the SweetAlert has been shown
+                    Session["SweetAlertShown"] = true;
+                }
+            }
         }
     }
 }
